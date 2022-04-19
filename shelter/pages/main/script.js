a07765a0d26b74d2b54ad06baa5432a70348ec53
Dinis <d.yudenka@yandex.by>
2022-04-19 10:24:56 +0300

    feat: add an adaptive for elements with a screen width less than 1280 px

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('.header__btn-burger');
 const navMenu = document.querySelector('.header__nav__items');
+const logo = document.querySelector('.header__logo');
 
         hamburger.addEventListener('click', () => {
             hamburger.classList.toggle('open');
             navMenu.classList.toggle('openMenu');
+            logo.classList.toggle('openMenu');
         });
 
 const navLinks = document.querySelectorAll('.header__nav__item .app__links');
@@ -12,5 +14,8 @@ const navLinks = document.querySelectorAll('.header__nav__item .app__links');
             navLink.addEventListener('click', () => {
                 hamburger.classList.toggle('open');
                 navMenu.classList.toggle('openMenu');
+                logo.classList.toggle('openMenu');
             })
         })
+
+alert('Всем привет, работа незавершена, пожалуйста если есть возможность проверьте ее завтра, очень сильно благодарен за понимание')
