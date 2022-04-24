e80cfb9f71736863e5b90fc61b797001f29abd1e
Dinis <d.yudenka@yandex.by>
2022-04-24 13:50:43 +0300

    feat: add a background and the ability to close the menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,19 +1,23 @@
 const hamburger = document.querySelector('.header__btn-burger');
 const navMenu = document.querySelector('.header__nav__items');
 const logo = document.querySelector('.header__logo');
-
-        hamburger.addEventListener('click', () => {
-            hamburger.classList.toggle('open');
-            navMenu.classList.toggle('openMenu');
-            // logo.classList.toggle('openMenu');
-        });
-
+const backgroundMenu = document.querySelector('.app__background__menu');
 const navLinks = document.querySelectorAll('.header__nav__item .app__links');
-        
-        navLinks.forEach((navLink) => {
-            navLink.addEventListener('click', () => {
-                hamburger.classList.toggle('open');
-                navMenu.classList.toggle('openMenu');
-                // logo.classList.toggle('openMenu');
-            })
-        })
+
+const changeClassElement = () => {
+    hamburger.classList.toggle('open');
+    navMenu.classList.toggle('openMenu');
+    backgroundMenu.classList.toggle('active');
+
+    if(navMenu.classList.contains('openMenu')) {
+        document.body.style.overflowY = 'hidden';
+    } else {
+        document.body.style.overflowY = '';
+    }
+}
+
+backgroundMenu.addEventListener('click', changeClassElement)
+hamburger.addEventListener('click', changeClassElement);
+navLinks.forEach((navLink) => {
+    navLink.addEventListener('click', changeClassElement)
+})
