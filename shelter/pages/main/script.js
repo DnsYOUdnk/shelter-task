dde6cbf74674aec4bb1592b55a49e8c411eb59c7
Dinis <d.yudenka@yandex.by>
2022-04-26 02:37:58 +0300

    feat: implement a carousel for the our pets friends block

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -4,7 +4,8 @@ const logo = document.querySelector('.header__logo');
 const backgroundMenu = document.querySelector('.app__background__menu');
 const navLinks = document.querySelectorAll('.header__nav__item .app__links');
 const ourFriendsCardsList = document.querySelector('.our__friends__petscards__items');
-const ourFriendsContentBtns = document.querySelectorAll('.our_friends__content__btn>button');
+const ourFriendsContentBtnLeft = document.querySelector('.our_friends__content__btn-left');
+const ourFriendsContentBtnRight = document.querySelector('.our_friends__content__btn-right');
 
 const changeClassElement = () => {
     hamburger.classList.toggle('open');
@@ -27,12 +28,16 @@ navLinks.forEach((navLink) => {
 const getShelterData = async () => {
     const response = await fetch('./../../assets/json/shelterPetsData.json');
     const  data = await response.json();
-    createPetsCards(data)
+    createPetsCards([...data, data[0]])
 }
 
+let newData = [];
+
 const createPetsCards = (data) => {
+    newData = data;
+
     let listPetsCards = '';
-    data.forEach(({name, img, id}) => {
+    newData.forEach(({name, img, id}) => {
         listPetsCards += `
                             <li class="our__friends__petscards__item">
                                 <div class="our__friends__petscards__item__image">
@@ -47,20 +52,42 @@ const createPetsCards = (data) => {
     })
     ourFriendsCardsList.innerHTML = listPetsCards;
 }
-
-let distance = 0;
-
-const changeViewPetsCards = (sideMove) => {
-    console.log(sideMove)
-    distance += sideMove === 'left' ? 900 : -900;
-    ourFriendsCardsList.style.left = distance + 'px';
+const moveLeft = () => {
+    ourFriendsCardsList.classList.add('left__move');
+    ourFriendsContentBtnLeft.removeEventListener('click', moveLeft);
+    ourFriendsContentBtnRight.removeEventListener('click', moveRight);
 }
 
-ourFriendsContentBtns.forEach( buttonMove => {
-    let sideMove = buttonMove.classList.contains('our_friends__content__btn-left') ? 'left' : 'right';
-    buttonMove.addEventListener('click', () => {
-        changeViewPetsCards(sideMove)
-    })
+const moveRight = () => {
+    ourFriendsCardsList.classList.add('right__move');
+    ourFriendsContentBtnRight.removeEventListener('click', moveRight);
+    ourFriendsContentBtnLeft.removeEventListener('click', moveLeft);
+}
+
+ourFriendsContentBtnRight.addEventListener('click', moveRight)
+ourFriendsContentBtnLeft.addEventListener('click', moveLeft)
+
+ourFriendsCardsList.addEventListener('animationend', (event) => {
+    let arrMoveCards = [],
+        arrCardsHidden = []; 
+
+    if (event.animationName === 'leftMove') {
+        ourFriendsCardsList.classList.remove('left__move')
+
+        arrMoveCards = newData.slice(0,3);
+        arrCardsHidden = newData.slice(3, newData.length-1).sort((a,b) => Math.random() - Math.random());
+    } else {
+        ourFriendsCardsList.classList.remove('right__move')
+
+        arrMoveCards = newData.slice(-3);
+        arrCardsHidden = newData.slice(1, -3).sort((a,b) => Math.random() - Math.random());
+    }
+
+    arrCardsHidden.push(arrCardsHidden[0])
+    createPetsCards(arrCardsHidden.slice(0,3).concat(arrMoveCards.concat(arrCardsHidden.slice(3))))
+
+    ourFriendsContentBtnRight.addEventListener('click', moveRight)
+    ourFriendsContentBtnLeft.addEventListener('click', moveLeft)
 })
 
 window.addEventListener('load', () => {
