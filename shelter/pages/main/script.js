6aebfaa4d101ae7ade498ba028000d54fcfa2ef8
Dinis <d.yudenka@yandex.by>
2022-04-28 03:42:35 +0300

    fix: adjusting the burger menu operation

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -9,26 +9,31 @@ const ourFriendsContentBtnLeft = document.querySelector('.our_friends__content__
 const ourFriendsContentBtnRight = document.querySelector('.our_friends__content__btn-right');
 
 const changeClassElement = () => {
+    hamburger.classList.toggle('open');
+    navMenu.classList.toggle('openMenu');
+    backgroundMenu.classList.toggle('active');
 
     if(navMenu.classList.contains('openMenu')) {
-        hamburger.classList.add('open');
-        navMenu.classList.add('openMenu');
-        backgroundMenu.classList.add('active');
         document.body.style.overflowY = 'hidden';
         navMenu.prepend(logo)
     } else {
-        hamburger.classList.remove('open');
-        navMenu.classList.remove('openMenu');
-        backgroundMenu.classList.remove('active');
         document.body.style.overflowY = '';
         headerWrapper.prepend(logo)
     }
 }
 
-backgroundMenu.addEventListener('click', changeClassElement)
+const removeClassElement = () => {
+    hamburger.classList.remove('open');
+    navMenu.classList.remove('openMenu');
+    backgroundMenu.classList.remove('active');
+    document.body.style.overflowY = '';
+    headerWrapper.prepend(logo);
+}
+
+backgroundMenu.addEventListener('click', removeClassElement)
 hamburger.addEventListener('click', changeClassElement);
 navLinks.forEach((navLink) => {
-    navLink.addEventListener('click', changeClassElement)
+    navLink.addEventListener('click', removeClassElement)
 })
 
 const getShelterData = async () => {
